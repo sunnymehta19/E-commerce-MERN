735fc828c9c3106ecda65e030f3a16e409f3e650
sunnymehta19 <whosunnymehta@gmail.com>
2026-01-12 21:58:18 +0530

    admin UI design

--- Frontend/src/components/admin-view/AdminLayout.jsx
+++ Frontend/src/components/admin-view/AdminLayout.jsx
@@ -1,15 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import AdminSidebar from './Sidebar'
 import AdminHeader from './Header'
 import { Outlet } from 'react-router-dom'
 
 const AdminLayout = () => {
+
+    const [OpenSidebar, setOpenSidebar] = useState(false);
+
     return (
         <>
             <div className="min-h-screen w-full flex ">
-                <AdminSidebar />
+                <AdminSidebar open={OpenSidebar} setOpen={setOpenSidebar} />
                 <div className="flex flex-col flex-1">
-                    <AdminHeader />
+                    <AdminHeader setOpen={setOpenSidebar} />
                     <main className=" bg-gray-100 flex-1 flex-col flex bg-muted/40  p-4 md:p-6 ">
                         <Outlet />
                     </main>
--- Frontend/src/components/admin-view/Header.jsx
+++ Frontend/src/components/admin-view/Header.jsx
@@ -4,7 +4,7 @@ import { useDispatch } from 'react-redux'
 import { logOutUser } from '../../store/slices/authSlice'
 import { Button } from '../ui/button'
 
-const AdminHeader = () => {
+const AdminHeader = ({ setOpen}) => {
 
   const dispatch = useDispatch();
 
@@ -14,12 +14,12 @@ const AdminHeader = () => {
 
   return (
     <header className="flex items-center justify-between px-4 py-3 bg-background ">
-      <Button onClick={() => setOpen(true)} className="lg:hidden sm:block">
-        <AlignJustify />
+      <button onClick={() => setOpen(true)} className="text-black lg:hidden sm:block">
+        <AlignJustify  />
         <span className="sr-only">Toggle Menu</span>
-      </Button>
+      </button>
       <div className="flex flex-1 justify-end">
-        <Button
+        <Button 
           onClick={handleLogout}
           className="inline-flex gap-2 items-center rounded-md px-4 py-2 text-sm font-medium shadow"
         >
